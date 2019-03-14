66a79c698fa3ae64493d3ed518fc9f757d9c90ee
Jared Grove <jaredkgrove@gmail.com>
2019-03-14 22:24:11 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -14,6 +14,6 @@ def find_the_cheese(ingredients)# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
   
-  for each ingredients
+  for each_with_index ingredients
   if array.include?()
 end
